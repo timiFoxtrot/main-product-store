0566e5024b9a7b3041101bf6b328121ef873de0f
Timi Ajibade <timiajibade24@gmail.com>
2025-04-01 11:22:12 +0100

    adds rate limiter

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,16 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import { trim_all } from "./common/middlewares/trim";
-// import { authenticate } from "./common/middlewares/auth";
+import rateLimit from "express-rate-limit";
 
 dotenv.config();
 const app = express();
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again later.",
+});
+app.use(limiter);
 app.use(express.json());
 app.use(cors());
 app.use(trim_all);
